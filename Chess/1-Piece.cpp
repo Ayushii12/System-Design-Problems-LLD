37fbe1d07cffffb09096d14bb379038369b5f6f2
Ayushi Patel <45871043+Ayushii12@users.noreply.github.com>
2025-03-20 15:42:48 -0500

    Create 2-Piece.cpp

---
+++ Chess/1-Piece.cpp
@@ -0,0 +1,27 @@
+#include "1-Color.cpp"
+
+class Piece {
+protected:
+    Color color;
+    int row;
+    int col;
+
+public:
+    Piece(Color color, int row, int col) : color(color), row(row), col(col) {}
+    virtual ~Piece() {}
+
+    // Getters
+    Color getColor() const { return color; }
+    int getRow() const { return row; }
+    int getCol() const { return col; }
+
+    // Setters
+    void setColor(Color newColor) { color = newColor; }
+    void setPosition(int r, int c) {
+        row = r;
+        col = c;
+    }
+    
+    // Pure virtual method to check if the move is legal
+    virtual bool canMove(int destRow, int destCol) = 0;
+};
