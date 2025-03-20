cda30e544030950a24fd18d237647015a3f15143
Ayushi Patel <45871043+Ayushii12@users.noreply.github.com>
2025-03-20 15:55:39 -0500

    Update 2a-King.cpp

--- Chess/2a-King.cpp
+++ Chess/2a-King.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 #include "2-Piece.cpp"
-
 using namespace std;
 
 class King : public Piece {
@@ -9,9 +8,9 @@ public:
     King(Color color, int row, int col) : Piece(color, row, col) {}
 
     // King can move one square in any direction
-    bool canMove(int destRow, int destCol) override {
-        int dRow = abs(destRow - row);
-        int dCol = abs(destCol - col);
+    bool canMove(int newRow, int newCol) override {
+        int dRow = abs(newRow - row);
+        int dCol = abs(newCol - col);
         return (dRow <= 1 && dCol <= 1);
     }
 };
