6942267c7d04524d1456853cee20302d6011b32d
Ayushi Patel <45871043+Ayushii12@users.noreply.github.com>
2025-03-23 18:27:04 -0500

    Update 1-User.cpp

--- File-Sharing-System/1-User.cpp
+++ File-Sharing-System/1-User.cpp
@@ -7,6 +7,8 @@ private:
     string username;
     string password;
     Role role;
+    size_t storageQuota;  // Maximum storage allowed in bytes
+    size_t usedStorage;   // Currently used storage in bytes
 
 public:
     User(const string& username, const string& password, Role role)
@@ -15,9 +17,27 @@ public:
     // Getters
     string getUsername() const { return username; }
     Role getRole() const { return role; }
+    size_t getStorageQuota() const { return storageQuota; }
+    size_t getUsedStorage() const { return usedStorage; }
     
     // Authentication method
     bool authenticate(const string& password) const {
         return this->password == password;
+    }
+
+    // Check if a file of a given size can be uploaded
+    bool canUpload(size_t fileSize) const {
+        return (usedStorage + fileSize) <= storageQuota;
+    }
+
+    // Update the storage after uploading a file
+    void addFile(size_t fileSize) {
+        if (!canUpload(fileSize)) throw runtime_error("Storage quota exceeded");
+        usedStorage += fileSize;
+    }
+
+    // Update the storage after deleting a file
+    void deleteFile(size_t fileSize) {
+        usedStorage -= fileSize;
     }
 };
